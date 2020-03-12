6096299425e958abd708da6d325e5e1890fd3f4f
Thomas Lundgren <thomasLundgren@live.com>
2020-03-12 14:18:57 +0100

    Remove ID from Event class, never used

--- src/se/hig/thlu/asteroids/observer/Event.java
+++ src/se/hig/thlu/asteroids/observer/Event.java
@@ -1,19 +1,11 @@
 package se.hig.thlu.asteroids.observer;
 
-import java.util.UUID;
-
 public class Event {
 
-	private final UUID id;
 	private final Object value;
 
 	public Event(Object value) {
 		this.value = value;
-		id = UUID.randomUUID();
-	}
-
-	public UUID getId() {
-		return id;
 	}
 
 	public Object getValue() {
